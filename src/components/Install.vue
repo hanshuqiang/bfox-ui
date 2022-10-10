6156c7b39b91f36eed96d3a1dd5f0b10f07a01a4
hanpanpan <hanpanpan@outlook.com>
2022-10-10 19:05:06 +0800

    修改了文档

--- src/components/Install.vue
+++ src/components/Install.vue
@@ -3,28 +3,28 @@
     <h1>安装</h1>
     <h2>环境支持</h2>
     <p>
-      Element Plus 可以在支持 ES2018 和 ResizeObserver 的浏览器上运行。 如果您确实需要支持旧版本的浏览器，请自行添加 Babel 和相应的 Polyfill 。
-
+      本项目是基于Element Plus再次封装，因此 和 Element Plus 的兼容性一致，可以在支持 ES2018 和 ResizeObserver 的浏览器上运行。 如果您确实需要支持旧版本的浏览器，请自行添加
+      Babel 和相应的 Polyfill 。
       由于 Vue 3 不再支持 IE11，Element Plus 也不再支持 IE 浏览器。
     </p>
     <h2>版本</h2>
-    <p>Element Plus 目前还处于快速开发迭代中。</p>
+    <p>BFoxUI 目前还处于快速开发迭代中。</p>
     <h2>使用包管理器</h2>
     <p>
-      我们建议您使用包管理器 (NPM, Yarn, pnpm) 安装 Element Plus, 然后您就可以使用打包工具，例如 Vite 和 webpack
+      我们建议您使用包管理器 (NPM, Yarn, pnpm) 安装 BFoxUI, 然后您就可以使用打包工具，例如 Vite 和 webpack
     </p>
     <pre class="language-html">
-<code class="language-bash">
-# 选择一个你喜欢的包管理器 
-# NPM 
-$ npm install element-plus --save 
-# Yarn 
-$ yarn add element-plus
-</code>
-         </pre>
+      <code class="language-bash">
+      # 选择一个你喜欢的包管理器 
+      # NPM 
+      $ npm i bfox-ui --save 
+      # Yarn 
+      $ yarn add bfox-ui
+      </code>
+    </pre>
     <h2>Hello World</h2>
-    <p>通过 CDN 的方式我们可以很容易地使用 Element Plus 写出一个 Hello world 页面。 在线演示</p>
-    <p>在 Crowdin 上编辑此页面 </p>
+    <p>通过 CDN 的方式我们可以很容易地使用BFoxUI 写出一个 Hello world 页面。 <a href="">在线演示</a></p>
+    <p>在 <a href="">Crowdin</a> 上编辑此页面 </p>
 
     <h1>用法</h1>
     <h2>完整引入</h2>
